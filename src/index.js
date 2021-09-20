5443a4144cf1aed0aaee6b47d3487cf83c251f08
Muzammil Hussain <me@mjeyz.com>
2021-09-20 22:09:54 +0500

    CRUD REACT APP

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'semantic-ui-css/semantic.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
+ReactDOM.render( <React.StrictMode>
+  <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
